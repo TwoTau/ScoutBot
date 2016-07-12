c7ab5eebe544a582d119ea103d1fe67453d0c86b
TwoTau <TwoTau@users.noreply.github.com>
2016-07-11 20:01:06 -0700

    Add endgame to game decode

--- app/providers/game-data-service/game-data-service.js
+++ app/providers/game-data-service/game-data-service.js
@@ -1,6 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
 
 @Injectable()
 
@@ -39,7 +37,7 @@ export class GameDataService {
     }
 
     base36ToDec(base36Num) {
-        return parseInt(base36Num, 36).toString(10);
+        return +(parseInt(base36Num, 36).toString(10));
     }
 
     // Returns a string
@@ -161,35 +159,44 @@ export class GameDataService {
         let colorNumberArray = ["Red 1", "Red 2", "Red 3", "Blue 1", "Blue 2", "Blue 3"];
         let scoutColorNumber = colorNumberArray[+encodedString.slice(4,5) - 1];
 
-        let foulDeadBot = this.hexToBoolArray(encodedString.slice(5, 6), 2);
+        let foulDeadBot = this.hexToBoolArray(encodedString.slice(5,6), 2);
         let foul = foulDeadBot[0];
         let deadBot = foulDeadBot[1];
 
-        let defensesNameArray = this.hexToBoolArray(encodedString.slice(6, 7), 4);
+        let defensesNameArray = this.hexToBoolArray(encodedString.slice(6,7), 4);
         let defenses = {
             a: {
-                name: defensesNameArray[0] ? "Cheval de Frise" : "Portcullis"
+                name: defensesNameArray[0] ? "Cheval de Frise" : "Portcullis",
+                makes: this.base36ToDec(encodedString.slice(9,10)),
+                misses: this.base36ToDec(encodedString.slice(10,11))
             },
             b: {
-                name: defensesNameArray[1] ? "Ramparts" : "Moat"
+                name: defensesNameArray[1] ? "Ramparts" : "Moat",
+                makes: this.base36ToDec(encodedString.slice(11,12)),
+                misses: this.base36ToDec(encodedString.slice(12,13))
             },
             c: {
-                name: defensesNameArray[2] ? "Sally Port" : "Drawbridge"
+                name: defensesNameArray[2] ? "Sally Port" : "Drawbridge",
+                makes: this.base36ToDec(encodedString.slice(13,14)),
+                misses: this.base36ToDec(encodedString.slice(14,15))
             },
             d: {
-                name: defensesNameArray[3] ? "Rough Terrain" : "Rock Wall"
+                name: defensesNameArray[3] ? "Rough Terrain" : "Rock Wall",
+                makes: this.base36ToDec(encodedString.slice(15,16)),
+                misses: this.base36ToDec(encodedString.slice(16,17))
             },
             e: {
-                name: "Lowbar"
+                name: "Lowbar",
+                made: this.binToBool(encodedString.slice(17,18))
             }
         };
 
-        let autonomousDefense = encodedString.slice(7, 8);
+        let autonomousDefense = encodedString.slice(7,8);
         let autonomousSuccessful = false;
         if(autonomousDefense === "N") {
             autonomousDefense = "None";
         } else {
-            let letterArray = ["a", "b", "c", "d", "e"];
+            let letterArray = ["a","b","c","d","e"];
             let autonomousDefenseLetter = letterArray[+autonomousDefense / 2];
             autonomousDefense = defenses[autonomousDefenseLetter];
             if(+autonomousDefense % 2 === 1) { // odd
@@ -197,14 +204,53 @@ export class GameDataService {
             }
         }
 
+        let autoShootingArray = this.hexToBoolArray(encodedString.slice(8,9), 3);
+        let autoBallGrabbed = autoShootingArray[0];
+        let autoHighGoal = autoShootingArray[1];
+        let autoLowGoal = autoShootingArray[2];
+
+        let teleopGoals = {
+            high: {
+                makes: this.base36ToDec(encodedString.slice(18,19)),
+                misses: this.base36ToDec(encodedString.slice(19,20))
+            },
+            low: {
+                makes: this.base36ToDec(encodedString.slice(20,21)),
+                misses: this.base36ToDec(encodedString.slice(21,22))
+            }
+        };
+
+        let endgameArray = this.hexToBoolArray(encodedString.slice(22,23), 2);
+        let endgame = {
+            challengedTower: endgameArray[0],
+            scaled: endgameArray[1]
+        };
+
+        let rolesArray = this.hexToBoolArray(encodedString.slice(23,24), 4);
+        let roles = {
+            highShooting: rolesArray[0],
+            lowShooting: rolesArray[1],
+            breaching: rolesArray[2],
+            defending: rolesArray[3]
+        };
+
+        let auto = {
+            defenseAttempted: autonomousDefense,
+            defensesSuccessful: autonomousSuccessful,
+            ballGrabbed: autoBallGrabbed,
+            highGoal: autoHighGoal,
+            lowGoal: autoLowGoal
+        };
+
         let final = {
             teamNumber: teamNumber,
             scoutColorNumber: scoutColorNumber,
             foul: foul,
             deadBot: deadBot,
             defenses: defenses,
-            autonomousDefense: autonomousDefense,
-            autonomousSuccessful: autonomousSuccessful
+            auto: auto,
+            teleopGoals: teleopGoals,
+            roles: roles
         };
 
         return final;
